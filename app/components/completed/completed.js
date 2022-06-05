8a4ef4c0a3d93bdbf06ab80107dd45cc4da06b2b
satanic-devil <freebiesinfo99@gmail.com>
2022-06-05 22:17:26 +0530

    fixing the component code

--- app/components/completed/completed.js
+++ app/components/completed/completed.js
@@ -2,5 +2,30 @@ import { $loadFile } from 'scripts/loadFile.js';
 
 const html = await $loadFile('./completed.html', import.meta.url);
 const css = await $loadFile('./completed.css', import.meta.url);
-console.log(html);
-console.log(css);
+
+class CompletedSection extends HTMLElement {
+
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+    }
+
+    connectedCallback() {
+        this.render();
+        this.setUp();
+    }
+
+    setUp() {
+
+    }   
+
+    render() {
+        
+        this.shadowRoot.innerHTML = `
+            <style>${css}</style>
+            ${html}
+        `;
+    }
+}
+
+customElements.define("completed-section", CompletedSection);
